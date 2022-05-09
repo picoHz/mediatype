bf9a0b9d48d4311f2677662d4c1a3910ed95d7e0
picoHz <53327867+picoHz@users.noreply.github.com>
2022-05-09 18:14:49 +0900

    MediaTypeBuf: support deserialization from an owned string

--- src/serde.rs
+++ src/serde.rs
@@ -2,6 +2,7 @@
 
 use super::{media_type::*, media_type_buf::*};
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
+use std::borrow::Cow;
 
 impl<'a> Serialize for MediaType<'a> {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
@@ -38,14 +39,15 @@ impl<'de> Deserialize<'de> for MediaTypeBuf {
         D: Deserializer<'de>,
     {
         use serde::de::Error;
-        let s: &str = Deserialize::deserialize(deserializer)?;
-        s.parse().map_err(Error::custom)
+        let s: Cow<str> = Deserialize::deserialize(deserializer)?;
+        Self::from_string(s.into_owned()).map_err(Error::custom)
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use serde_json::Value;
     use std::str::FromStr;
 
     #[test]
@@ -53,6 +55,14 @@ mod tests {
         let original = MediaTypeBuf::from_str("image/svg+xml; charset=UTF-8;").unwrap();
         let json = serde_json::to_string(&original).unwrap();
         let decoded: MediaType = serde_json::from_str(&json).unwrap();
+        assert_eq!(original, decoded);
+    }
+
+    #[test]
+    fn serde_from_value() {
+        let original = MediaTypeBuf::from_str("image/svg+xml; charset=UTF-8;").unwrap();
+        let value = Value::String(original.as_str().into());
+        let decoded: MediaTypeBuf = serde_json::from_value(value).unwrap();
         assert_eq!(original, decoded);
     }
 }
